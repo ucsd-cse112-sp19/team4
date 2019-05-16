e24d7c3af9d122d934836549362b460eb34681b2
christopherthomas226 <cat044@ucsd.edu>
2019-05-16 00:52:31 -0700

    added some simple tests

--- component/slider/test.js
+++ component/slider/test.js
@@ -5,43 +5,53 @@ suite('slider-component', function () {
     const component = document.createElement('slider')
     document.body.append(component)
     assert.isDefined(component)
-    assert.equal(component.language, 'en')
-    assert.equal(component.rainbow, 'false')
+    assert.equal(component.max, '100')
+    assert.equal(component.min, '0')
+    assert.equal(component.show_input, 'false')
+    assert.equal(component.value, '42')
   })
-  // Tests to see if the default language value is en
-  test('default language is en', function () {
+  // Tests to see if the default max value is 100
+  test('default max is 100', function () {
     const component = document.createElement('slider')
     document.body.append(component)
     assert.isDefined(component)
-    assert.equal(component.language, 'en')
+    assert.equal(component.max, '100')
   })
-  // Tests to see if the default rainbow value is false
-  test('default rainbow is false', function () {
+  // Tests to see if the default min value is 0
+  test('default min is 0', function () {
     const component = document.createElement('slider')
     document.body.append(component)
     assert.isDefined(component)
-    assert.equal(component.rainbow, 'false')
+    assert.equal(component.min, '0')
   })
-  // Tests to see if it handles changing the language value
-  test('changing language works', function () {
+  // Tests to see if the default show_input value is false
+  test('default show_input is false', function () {
     const component = document.createElement('slider')
     document.body.append(component)
     assert.isDefined(component)
-    assert.equal(component.language, 'en')
-    component.language = 'fr'
-    assert.equal(component.language, 'fr')
-    component.language = 'en'
-    assert.equal(component.language, 'en')
+    assert.equal(component.show_input, 'false')
   })
-  // Tests to see if it handles changing the rainbow value
-  test('changing rainbow works', function () {
+
+  // Tests to see if it handles changing the slider value
+  test('changing slider value works', function () {
     const component = document.createElement('slider')
     document.body.append(component)
     assert.isDefined(component)
-    assert.equal(component.rainbow, 'false')
-    component.rainbow = 'true'
-    assert.equal(component.rainbow, 'true')
-    component.rainbow = 'false'
-    assert.equal(component.rainbow, 'false')
+    assert.equal(component.value, '42')
+    component.value = component.min
+    assert.equal(component.value, component.min)
+    component.language = component.max
+    assert.equal(component.language, component.max)
+  })
+  // Tests to see if it handles changing the show_input value
+  test('changing show_input works', function () {
+    const component = document.createElement('slider')
+    document.body.append(component)
+    assert.isDefined(component)
+    assert.equal(component.show_input, 'false')
+    component.show_input = 'true'
+    assert.equal(component.show_input, 'true')
+    component.show_input = 'false'
+    assert.equal(component.show_input, 'false')
   })
 })
