bff116a57efeccc7d28b9afc742a4d567d05f943
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-05-21 09:36:47 -0700

    removed sel standalone

--- wdio.conf.js
+++ wdio.conf.js
@@ -194,7 +194,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['sauce', 'selenium-standalone'],
+  services: ['sauce'],
   sauceConnect: true,
   //
   // Framework you want to run your specs with.
