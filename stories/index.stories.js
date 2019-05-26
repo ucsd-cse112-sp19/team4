5f0f2a76169e4affa4e6b6951306d9e21818719e
christopherthomas226 <cat044@ucsd.edu>
2019-05-26 14:11:12 -0700

    added slide story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,19 @@
 import { storiesOf } from '@storybook/html'
 import '../component/button/button-component.js'
+import '../component/slider/slider-component.js'
 
 storiesOf('button-component', module)
   .add('button', () => {
     const el = document.createElement('button-component')
     el.type = 'primary'
     el.size = 'medium'
+    document.body.append(el)
+    return el
+  })
+
+storiesOf('slider-component', module)
+  .add('button', () => {
+    const el = document.createElement('slider-component')
     document.body.append(el)
     return el
   })
