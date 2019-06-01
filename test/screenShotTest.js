876e04940ccffba4df734301729025a29af1f5a7
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-01 15:57:21 -0700

    removed the element id feature

--- test/screenShotTest.js
+++ test/screenShotTest.js
@@ -9,7 +9,7 @@ describe('Example', () => {
     browser.saveScreen('examplePaged', { /* some options */ })
 
     // Save an element
-    browser.saveElement($('#element-id'), 'firstButtonElement', { /* some options */ })
+    // browser.saveElement($('#element-id'), 'firstButtonElement', { /* some options */ })
 
     // Save a full page screens
     browser.saveFullPageScreen('fullPage', { /* some options */ })
@@ -20,7 +20,7 @@ describe('Example', () => {
     expect(browser.checkScreen('examplePaged', { /* some options */ })).toEqual(0)
 
     // Check an element
-    expect(browser.checkElement($('#element-id'), 'firstButtonElement', { /* some options */ })).toEqual(0)
+    // expect(browser.checkElement($('#element-id'), 'firstButtonElement', { /* some options */ })).toEqual(0)
 
     // Check a full page screens
     expect(browser.checkFullPageScreen('fullPage', { /* some options */ })).toEqual(0)
