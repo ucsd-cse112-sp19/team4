262b0bc04411ad042c4e4a8cee0cc1acbd99a6b1
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-05 21:51:16 -0700

    testing if cascader works with clicking on sauce

--- test/testSlider.js
+++ test/testSlider.js
@@ -7,8 +7,8 @@ describe('Testing functionallity for sliders ', () => {
   it('Testing the top row checking if the button\'s interact correctly', () => {
     browser.pause(200)
 
-    const res1 = $('#sluno')
-    const res2 = $('#sldos')
+    // const res1 = $('#sluno')
+    // const res2 = $('#sldos')
     browser.pause(200)
     browser.takeSnapshot('slider page')
     browser.pause(200)
