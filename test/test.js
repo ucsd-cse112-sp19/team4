ef66fc24de6fa97dba8f79314d251d1b5442a754
erick <ericksotoe@gmail.com>
2019-06-05 18:21:02 -0700

    testing the clicked button test

--- test/test.js
+++ test/test.js
@@ -26,6 +26,7 @@ describe('Testing functionallity for buttons ', () => {
     browser.pause(250)
     res7.click()
     browser.pause(3000)
+    browser.takeSnapshot('clicked button page')
     // browser.takeSnapshot('clicked button')
 
     // browser.pause(200)
