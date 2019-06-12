95a4130f5f9be80ddd4f76806a764d406c5bbad2
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-12 14:48:13 -0700

    checking if problem is resolve, also only running 1 test

--- wdio.conf.js
+++ wdio.conf.js
@@ -33,7 +33,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './test/*.js'
+    './test/test.js'
   ],
   // Patterns to exclude.
   exclude: [
