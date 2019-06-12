564207959eaa5d4472856b5df750d7d253fe476f
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-12 15:37:46 -0700

    only testing button

--- wdio.conf.js
+++ wdio.conf.js
@@ -33,7 +33,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './test/*.js'
+    './test/testButton.js'
   ],
   // Patterns to exclude.
   exclude: [
