8c847f6ed879b835427bbc1a09873231172546ed
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-12 14:41:29 -0700

    fixed bug with selector id

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ beforeEach(async () => {
 // this will test the button component to ensure that it is visable in different browswers
 describe('Testing integration for buttons ', () => {
   it('Checking if the buttons are displayed correctly on the browser', () => {
-    cosnt res = $('uno')
+    cosnt res = $('#uno')
     browser.pause(300)
     res.moveTo()
     // browser.takeSnapshot('button page')
