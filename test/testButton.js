f05ed9fe50530b10425c020f0e5cd58b9589a134
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-06-12 17:23:02 -0700

    testing to see if selector works with safari

--- test/testButton.js
+++ test/testButton.js
@@ -1,4 +1,4 @@
-/* global beforeEach describe it */
+/* global beforeEach describe it $ */
 beforeEach(async () => {
   // this will set the endpoint to be the button component page
   await browser.url('/team4/team4/button/')
@@ -9,7 +9,9 @@ describe('Testing integration for buttons ', () => {
   it('Checking if the buttons are displayed correctly on the browser', () => {
     // this pause is used to ensure that the webpage is loaded before starting ui test
     browser.pause(300)
+    const res = $('#uno')
     browser.takeSnapshot('button page')
+    res.moveTo()
     browser.pause(200)
   })
 })
